045ddd8c7c51ce6524886b20ca7404f46805b666
Hieu Phan <64969360+Pogchop@users.noreply.github.com>
2020-05-12 19:53:10 +0700

    Create Baitap_1.c

---
+++ Baitap_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char class[20]={'T','1','0','0','4','L'};
+    char Rolno[20]={'B','0','0','0','1'};
+    char fullname[20]={'N','g','u','y','e','n',' ','V','a','n',' ','A'};
+    char email[30]={'a','n','v','@','f','p','t','.','a','p','t','e','c','h','.','a','c','.','v','n'};
+    char phone[20]={'0','9','1','2','3','4','5','6','7','8'};
+    printf("Class: %s\n",class);
+    printf("Rolno: %s\n",Rolno);
+    printf("Full name: %s\n",fullname);
+    printf("Email: %s\n",email);
+    printf("Phone: %s",phone);
+    return 0;
+}
